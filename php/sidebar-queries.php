0c686f061b04487a08299176b72623b3d01ff513
collsmaq <colleen.maquiling>
2015-04-11 14:32:35 -0400

    queries

--- php/sidebar-queries.php
+++ php/sidebar-queries.php
@@ -1,23 +1,23 @@
 <?php
 
 if("All" == $_GET['type'])
-	echo "<li role=\"presentation\" class=\"active\"><a href=\"queriesresults.php?type=All\">All</a></li>";
+	echo "<li role=\"presentation\" class=\"active\"><a href=\"queries.php?type=All\">All</a></li>";
 else
-	echo "<li role=\"presentation\"><a href=\"menuitems.php?type=queriesresults.php\">All</a></li>";
+	echo "<li role=\"presentation\"><a href=\"queries.php?type=All\">All</a></li>";
 
-if("Restaurants and menus" == $_GET['type'])
-	echo "<li role=\"presentation\" class=\"active\"><a href=\"queriesresults.php?type=restaurantsmenus.php\">Restaurants and Menus</a></li>";
+if("Restaurants and Menus" == $_GET['type'])
+	echo "<li role=\"presentation\" class=\"active\"><a href=\"queries.php?type=Restaurants and Menus\">Restaurants and Menus</a></li>";
 else
-	echo "<li role=\"presentation\"><a href=\"menuitems.php?type=restaurantsmenus.php\">Restaurants and Menus</a></li>";
+	echo "<li role=\"presentation\"><a href=\"queries.php?type=Restaurants and Menus\">Restaurants and Menus</a></li>";
 
 if("Ratings of Restaurants" == $_GET['type'])
-	echo "<li role=\"presentation\" class=\"active\"><a href=\"queriesresults.php?type=ratingsrestaurants.php\">Restaurants and Menus</a></li>";
+	echo "<li role=\"presentation\" class=\"active\"><a href=\"queries.php?type=Ratings of Restaurants\">Restaurants and Menus</a></li>";
 else
-	echo "<li role=\"presentation\"><a href=\"menuitems.php?type=ratingsrestaurants.php\">Restaurants and Menus</a></li>";
+	echo "<li role=\"presentation\"><a href=\"queries.php?type=Ratings of Restaurants\">Restaurants and Menus</a></li>";
 
-if("Raters and their ratings" == $_GET['type'])
-	echo "<li role=\"presentation\" class=\"active\"><a href=\"queriesresults.php?type=ratersratings\">Raters and their ratings</a></li>";
+if("Raters and Their Ratings" == $_GET['type'])
+	echo "<li role=\"presentation\" class=\"active\"><a href=\"queries.php?type=Raters and Their Ratings\">Raters and Their Ratings</a></li>";
 else
-	echo "<li role=\"presentation\"><a href=\"menuitems.php?type=ratersratings.php\">Raters and their ratings</a></li>";
+	echo "<li role=\"presentation\"><a href=\"queries.php?type=Raters and Their Ratings\">Raters and Their Ratings</a></li>";
 
 ?>
