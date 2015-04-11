aac2265e95d36bdc54e6e52bb01d1739d8ed8460
emnes <mazshar@hotmail.co.uk>
2015-04-11 14:31:17 -0400

    dropdown for restaurants sorting done

--- restaurants.php
+++ restaurants.php
@@ -62,20 +62,26 @@
       </div>
         </div>
 
-    <div class="col-md-10">
+    <div class="col-md-9">
         <div class="page-header">
           <?php 
             $type = $_GET['type']; 
             $title = $type." Restaurants"; 
             echo "<h2>".$title."<h2>"; 
           ?>
-          <h5>Sort by: <div class="dropdown">
+          <h5><div class="dropdown">
               <button class="btn btn-default dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-expanded="true">
-               Popularity
+               Sort By
               <span class="caret"></span>
               </button>
               <ul class="dropdown-menu" role="menu" aria-labelledby="dropdownMenu1">
-              <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Popularity</a></li>
+              <li role="presentation"><a role="menuitem" tabindex="-1" href="">Most Popular</a></li>
+              <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Name</a></li>
+              <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Date Opened</a></li>
+              <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Best Price</a></li>
+              <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Best Mood</a></li>
+              <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Best Food</a></li>
+              <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Best Staff</a></li>
               </ul>
               </div>
           </h5>
@@ -96,11 +102,11 @@
                 if($row[8]==0)
                   $likeness = "Not yet rated";
                 else
-                  $likeness = $row[8]." rating";
+                  $likeness = $row[8]." <span class=\"glyphicon glyphicon-star\" aria-hidden=\"true\"></span>";
                 echo "<div class=\"row clearfix\">
-                <a href=\"restaurantprofile.php?locationid=".$row[0]."\">".$row[1]." (".$likeness.")</a>
+                <a href=\"restaurantprofile.php?locationid=".$row[0]."\">".$row[1]."&nbsp;&nbsp;"."</a><h3>".$likeness."</h3>
                 <h5> Address: ".$row[5]."</h5>
-                <h5> Phone: ".$row[4]."</h5>
+                <h5> Phone: ".$row[4]."<&nbsp;
                 <h5> Opening hours: ".$row[6]." to ".$row[7]."</h5>
                 <h5> Open since: ".$row[2]."</h5>
                 <h5> Manager name: ".$row[3]."</h5>
